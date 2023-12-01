fc8096961483d714e1a46e46276f8a5cdbd3612f
seokhochoi <seokho9473@gmail.com>
2023-12-01 20:34:09 +0900

    [Style]: 인스턴스의 타입체크 함수명 변경

--- javascript/data-type/immutable-object/tdd-deep-copy.js
+++ javascript/data-type/immutable-object/tdd-deep-copy.js
@@ -1,31 +1,31 @@
 import { createTestObject } from './test-object.js';
 
-const checkInstance = (instance, type) => {
+const isInstanceType = (instance, type) => {
   return Object.prototype.toString.call(instance) === type;
 };
 
 export const deepCopy = (target) => {
-  if (checkInstance(target, '[object Array]')) {
+  if (isInstanceType(target, '[object Array]')) {
     return [];
   }
 
-  if (checkInstance(target, '[object Date]')) {
+  if (isInstanceType(target, '[object Date]')) {
     return new Date(target);
   }
 
-  if (checkInstance(target, '[object RegExp]')) {
+  if (isInstanceType(target, '[object RegExp]')) {
     return new RegExp(target);
   }
 
-  if (checkInstance(target, '[object Map]')) {
+  if (isInstanceType(target, '[object Map]')) {
     return new Map();
   }
 
-  if (checkInstance(target, '[object Set]')) {
+  if (isInstanceType(target, '[object Set]')) {
     return new Set();
   }
 
-  if (checkInstance(target, '[object Object]')) {
+  if (isInstanceType(target, '[object Object]')) {
     return {};
   }
 
