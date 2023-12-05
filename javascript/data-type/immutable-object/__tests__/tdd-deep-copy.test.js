417d4d3132be092344544416a4a586fa4c1f6501
seokhochoi <seokho9473@gmail.com>
2023-12-05 19:46:37 +0900

    [Feat]: 테스트를 통과하기 위해 간단한 논리로 작성된 deepCopy 함수 구현 완료

--- javascript/data-type/immutable-object/__tests__/tdd-deep-copy.test.js
+++ javascript/data-type/immutable-object/__tests__/tdd-deep-copy.test.js
@@ -1,6 +1,7 @@
 // import { createTestObject } from '../test-object';
 
 import { deepCopy } from '../tdd-deep-copy';
+import { createTestObject } from '../test-object';
 
 /**
  * 복사된 객체의 중첩된 객체도 원본과 다른 참조를 가지는지 확인
@@ -13,12 +14,10 @@ import { deepCopy } from '../tdd-deep-copy';
  *  두 객체의 타입도 동일한지를 확인합니다.
  */
 
-const createTestObject = {};
 describe('deepCopy', () => {
   // given
   describe('Map을 포함하는 테스트 오브젝트에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = new Map();
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -27,17 +26,16 @@ describe('deepCopy', () => {
       // then
       it('복사된 객체를 반환합니다.', () => {
         expect(deepCopyResult.map).toStrictEqual(originalObject.map);
-        // expect(deepCopyResult.map.get('key2')).not.toBe(
-        //   originalObject.map.get('key2')
-        // );
+        expect(deepCopyResult.map.get('key2')).not.toBe(
+          originalObject.map.get('key2')
+        );
       });
     });
   });
 
   // given
   describe('Set을 포함하는 테스트 오브젝트에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = new Set();
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -46,40 +44,39 @@ describe('deepCopy', () => {
       // then
       it('복사된 객체를 반환합니다.', () => {
         expect(deepCopyResult.set).toStrictEqual(originalObject.set);
-        // expect(deepCopyResult.set).not.toBe(originalObject.set);
-        // let isDeepCopy = true;
-        // originalObject.set.forEach((element) => {
-        //   if (typeof element === 'object' && element !== null) {
-        //     let hasNestedSet = deepCopyResult.set.has(element);
-        //     if (!hasNestedSet) {
-        //       for (let deepCopyElement of deepCopyResult.set) {
-        //         if (
-        //           typeof deepCopyElement === 'object' &&
-        //           deepCopyElement !== null
-        //         ) {
-        //           if (
-        //             JSON.stringify(element) === JSON.stringify(deepCopyElement)
-        //           ) {
-        //             hasNestedSet = true;
-        //             break;
-        //           }
-        //         }
-        //       }
-        //     }
-        //     if (!hasNestedSet) {
-        //       isDeepCopy = false;
-        //     }
-        //   }
-        // });
-        // expect(isDeepCopy).toBe(true);
+        expect(deepCopyResult.set).not.toBe(originalObject.set);
+        let isDeepCopy = true;
+        originalObject.set.forEach((element) => {
+          if (typeof element === 'object' && element !== null) {
+            let hasNestedSet = deepCopyResult.set.has(element);
+            if (!hasNestedSet) {
+              for (let deepCopyElement of deepCopyResult.set) {
+                if (
+                  typeof deepCopyElement === 'object' &&
+                  deepCopyElement !== null
+                ) {
+                  if (
+                    JSON.stringify(element) === JSON.stringify(deepCopyElement)
+                  ) {
+                    hasNestedSet = true;
+                    break;
+                  }
+                }
+              }
+            }
+            if (!hasNestedSet) {
+              isDeepCopy = false;
+            }
+          }
+        });
+        expect(isDeepCopy).toBe(true);
       });
     });
   });
 
   // given
   describe('Array를 포함하는 테스트 오브젝트에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = { arr: new Array() };
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -88,15 +85,14 @@ describe('deepCopy', () => {
       // then
       it('복사된 객체를 반환합니다.', () => {
         expect(deepCopyResult.array).toStrictEqual(originalObject.array);
-        // expect(deepCopyResult.array[3]).not.toBe(originalObject.array[3]);
+        expect(deepCopyResult.array[3]).not.toBe(originalObject.array[3]);
       });
     });
   });
 
   // given
   describe('Date를 포함하는 테스트 오브젝트에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = new Date();
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -112,8 +108,7 @@ describe('deepCopy', () => {
 
   // given
   describe('RegExp를 포함하는 테스트 오브젝트에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = new RegExp();
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -129,8 +124,7 @@ describe('deepCopy', () => {
 
   // given
   describe('다른 경우에 대해서', () => {
-    // const originalObject = createTestObject();
-    const originalObject = {};
+    const originalObject = createTestObject();
 
     // when
     describe('깊은 복사가 되었다면', () => {
@@ -144,9 +138,9 @@ describe('deepCopy', () => {
         // 참조가 동일한지 확인
         expect(deepCopyResult).not.toBe(originalObject);
 
-        // expect(deepCopyResult.object.prop3).not.toBe(
-        //   originalObject.object.prop3
-        // );
+        expect(deepCopyResult.object.prop3).not.toBe(
+          originalObject.object.prop3
+        );
       });
     });
   });
