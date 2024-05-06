e6961ce93abf19d6cd7264dfa75e3423b1ff125c
seokhochoi <seokho9473@gmail.com>
2024-05-06 23:14:44 +0900

    test: 코드 수정을 위한 테스트 코드 수정

--- algorithm/programmers/high-score-kit/stack-queue/2-level/stock-prices/index.test.js
+++ algorithm/programmers/high-score-kit/stack-queue/2-level/stock-prices/index.test.js
@@ -1,42 +1,49 @@
-import { calculateDuration, calculateDurations, solution } from '.';
+import {
+  calculateDuration,
+  calculateFinalDurations,
+  calculateDurations,
+  solution,
+} from './index';
 
-describe('calculateDuration 함수 테스트', () => {
-  it('특정 인덱스에서 가격이 떨어지지 않은 기간을 올바르게 계산합니다.', () => {
+describe('calculateDuration 함수', () => {
+  it('주어진 가격 배열에서 각 시점마다 가격이 떨어지지 않은 기간을 계산하고, 원본 배열을 변경한다.', () => {
+    // Given
     const prices = [1, 2, 3, 2, 3];
+    const stack = [0, 1, 2];
+    const answer = new Array(prices.length).fill(0);
 
-    // 특정 인덱스와 해당 인덱스에서의 기대되는 결과
-    const testCases = [
-      { index: 0, expected: 4 }, // prices[0] = 1, 1이 떨어지지 않은 기간은 4초입니다.
-      { index: 1, expected: 3 }, // prices[1] = 2, 2가 떨어지지 않은 기간은 3초입니다.
-      { index: 2, expected: 1 }, // prices[2] = 3, 3이 떨어지지 않은 기간은 1초입니다.
-      { index: 3, expected: 1 }, // prices[3] = 2, 2가 떨어지지 않은 기간은 1초입니다.
-      { index: 4, expected: 0 }, // prices[4] = 3, 3이 떨어지지 않은 기간은 0초입니다.
-    ];
+    // When
+    calculateDuration(prices, 3, stack, answer);
 
-    // 각 테스트 케이스 실행 및 결과 검증
-    testCases.forEach(({ index, expected }) => {
-      const result = calculateDuration(prices, index);
-      expect(result).toEqual(expected);
-    });
+    // Then
+    expect(answer).toEqual([0, 0, 1, 0, 0]); // 원본 배열이 기대한 대로 변경되었는지 확인
   });
 });
 
-describe('calculateDurations 함수 테스트', () => {
-  it('주어진 주식 가격 배열에 대해 가격이 떨어지지 않은 기간을 올바르게 계산합니다.', () => {
+describe('calculateFinalDurations 함수', () => {
+  it('가격이 떨어지지 않은 기간을 마무리 짓고, 원본 배열을 변경한다.', () => {
+    // Given
     const prices = [1, 2, 3, 2, 3];
-    const expected = [4, 3, 1, 1, 0];
+    const stack = [0, 1, 3, 4];
+    const answer = [0, 0, 1, 0, 0];
 
+    // When
+    calculateFinalDurations(prices, stack, answer);
+
+    // Then
+    expect(answer).toEqual([4, 3, 1, 1, 0]); // 원본 배열이 기대한 대로 변경되었는지 확인
+  });
+});
+
+describe('calculateDurations 함수', () => {
+  it('각 시점마다 가격이 떨어지지 않은 기간을 계산하고 마무리 짓고, 원본 배열을 변경한다.', () => {
+    // Given
+    const prices = [1, 2, 3, 2, 3];
+
+    // When
     const result = calculateDurations(prices);
-    expect(result).toEqual(expected);
-  });
-});
 
-describe('solution 함수 테스트', () => {
-  it('주어진 주식 가격 배열에 대해 가격이 떨어지지 않은 기간을 올바르게 계산합니다.', () => {
-    const prices = [1, 2, 3, 2, 3];
-    const expected = [4, 3, 1, 1, 0];
-
-    const result = solution(prices);
-    expect(result).toEqual(expected);
+    // Then
+    expect(result).toEqual([4, 3, 1, 1, 0]); // 반환 값이 기대한 대로 나왔는지 확인
   });
 });
