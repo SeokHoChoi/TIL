01e398c8de71fe6c39b70958fd4a84c3c2511643
seokhochoi <seokho9473@gmail.com>
2024-05-09 00:42:54 +0900

    feat: k번째수 문제를 선언적으로 리팩토링

---
+++ algorithm/programmers/high-score-kit/sort/kth-number/index.js
@@ -0,0 +1,21 @@
+export function sliceAndSort(array, start, end) {
+  return array.slice(start - 1, end).sort((a, b) => a - b);
+}
+
+export function findKthNumber(sortedArray, k) {
+  return sortedArray[k - 1];
+}
+
+export function solution(array, commands) {
+  const answer = [];
+
+  for (const command of commands) {
+    const [start, end, pick] = command;
+    const sortedArray = sliceAndSort(array, start, end);
+    const kthNumber = findKthNumber(sortedArray, pick);
+
+    answer.push(kthNumber);
+  }
+
+  return answer;
+}
