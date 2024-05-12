eda6332d76aa3b7eea477070441d47f4d9d0354a
seokhochoi <seokho9473@gmail.com>
2024-05-12 21:18:51 +0900

    refactor: 시멘틱한 변수명 사용 및 선언적 코드로 리팩토링

--- algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.js
+++ algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.js
@@ -1,37 +1,38 @@
-function solution(answers) {
-  const answer = [];
-  const arr1 = [1, 2, 3, 4, 5];
-  const arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  const counts = [
+export function solution(answers) {
+  const correctCounts = [
     { id: 1, count: 0 },
     { id: 2, count: 0 },
     { id: 3, count: 0 },
   ];
 
-  answers.forEach((num, index) => {
-    if (num === arr1[index % arr1.length]) {
-      counts[0].count++;
+  const pattern1 = [1, 2, 3, 4, 5];
+  const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  const pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+
+  answers.forEach((answer, index) => {
+    if (answer === pattern1[index % pattern1.length]) {
+      correctCounts[0].count++;
     }
 
-    if (num === arr2[index % arr2.length]) {
-      counts[1].count++;
+    if (answer === pattern2[index % pattern2.length]) {
+      correctCounts[1].count++;
     }
 
-    if (num === arr3[index % arr3.length]) {
-      counts[2].count++;
-    }
-  });
-
-  counts.sort((a, b) => b.count - a.count);
-
-  const maxCount = counts[0].count;
-  counts.forEach((obj) => {
-    if (obj.count === maxCount) {
-      answer.push(obj.id);
+    if (answer === pattern3[index % pattern3.length]) {
+      correctCounts[2].count++;
     }
   });
 
-  return answer;
+  correctCounts.sort((a, b) => b.count - a.count);
+
+  const maxCount = correctCounts[0].count;
+
+  // 모든 수포자가 문제를 하나도 맞추지 못한 경우
+  if (maxCount === 0) return [];
+
+  const highestScorers = correctCounts
+    .filter((person) => person.count === maxCount)
+    .map((person) => person.id);
+
+  return highestScorers;
 }
