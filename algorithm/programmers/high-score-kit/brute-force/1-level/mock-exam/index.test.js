c86a6d79e836dc3bd77dda3d9f9c077fc12911a3
seokhochoi <seokho9473@gmail.com>
2024-05-12 21:28:14 +0900

    test: 각 기능에 대한 테스트 추가

--- algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.test.js
+++ algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.test.js
@@ -1,4 +1,55 @@
-import { solution } from './index';
+import { countCorrectAnswers, getHighestScorers, solution } from './index';
+
+describe('countCorrectAnswers 함수', () => {
+  it('정답 배열과 패턴이 주어졌을 때, 정확히 맞은 개수를 반환해야 합니다.', () => {
+    const answers = [1, 2, 3, 4, 5];
+    const pattern = [1, 2, 3, 4, 5];
+    const correctCount = countCorrectAnswers(answers, pattern);
+    expect(correctCount).toBe(5);
+  });
+
+  it('정답 배열과 패턴이 주어졌을 때, 정확히 맞은 개수를 반환해야 합니다.', () => {
+    const answers = [1, 3, 2, 4, 2];
+    const pattern = [2, 1, 2, 3, 2, 4, 2, 5];
+    const correctCount = countCorrectAnswers(answers, pattern);
+    expect(correctCount).toBe(2);
+  });
+
+  it('정답 배열과 패턴이 주어졌을 때, 정확히 맞은 개수를 반환해야 합니다.', () => {
+    const answers = [
+      3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5,
+    ];
+    const pattern = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const correctCount = countCorrectAnswers(answers, pattern);
+    expect(correctCount).toBe(20);
+  });
+});
+
+describe('getHighestScorers 함수', () => {
+  it('각 수포자의 정답 개수 배열이 주어졌을 때, 가장 많이 맞은 사람의 번호를 배열로 반환해야 합니다.', () => {
+    const correctCounts = [3, 5, 3];
+    const highestScorers = getHighestScorers(correctCounts);
+    expect(highestScorers).toEqual([2]);
+  });
+
+  it('각 수포자의 정답 개수 배열이 주어졌을 때, 가장 많이 맞은 사람의 번호를 배열로 반환해야 합니다.', () => {
+    const correctCounts = [4, 4, 4];
+    const highestScorers = getHighestScorers(correctCounts);
+    expect(highestScorers).toEqual([1, 2, 3]);
+  });
+
+  it('각 수포자의 정답 개수 배열이 주어졌을 때, 가장 많이 맞은 사람의 번호를 배열로 반환해야 합니다.', () => {
+    const correctCounts = [2, 0, 3];
+    const highestScorers = getHighestScorers(correctCounts);
+    expect(highestScorers).toEqual([3]);
+  });
+
+  it('각 수포자의 정답 개수 배열이 주어졌을 때, 모든 수포자가 문제를 하나도 맞추지 못한 경우 빈 배열을 반환해야 합니다.', () => {
+    const correctCounts = [0, 0, 0];
+    const highestScorers = getHighestScorers(correctCounts);
+    expect(highestScorers).toEqual([]);
+  });
+});
 
 describe('solution 함수', () => {
   it('정답 배열이 [1, 2, 3, 4, 5]인 경우, [1]을 반환해야 합니다.', () => {
