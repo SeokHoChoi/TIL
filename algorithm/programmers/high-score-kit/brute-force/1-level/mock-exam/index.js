579f7180c423473bea8d906bf7449ec822b498dd
seokhochoi <seokho9473@gmail.com>
2024-05-12 21:33:10 +0900

    refactor: 손쉬운 문제 파악을 위해 정답을 계산하는 데 필요한 도우미 함수와 가장 높은 점수를 받는 사람을 찾는 데 도움이 되는 함수를 추출

--- algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.js
+++ algorithm/programmers/high-score-kit/brute-force/1-level/mock-exam/index.js
@@ -1,38 +1,34 @@
-export function solution(answers) {
-  const correctCounts = [
-    { id: 1, count: 0 },
-    { id: 2, count: 0 },
-    { id: 3, count: 0 },
-  ];
-
-  const pattern1 = [1, 2, 3, 4, 5];
-  const pattern2 = [2, 1, 2, 3, 2, 4, 2, 5];
-  const pattern3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  answers.forEach((answer, index) => {
-    if (answer === pattern1[index % pattern1.length]) {
-      correctCounts[0].count++;
+export function countCorrectAnswers(answers, pattern) {
+  return answers.reduce((count, answer, index) => {
+    if (answer === pattern[index % pattern.length]) {
+      count++;
     }
+    return count;
+  }, 0);
+}
 
-    if (answer === pattern2[index % pattern2.length]) {
-      correctCounts[1].count++;
-    }
-
-    if (answer === pattern3[index % pattern3.length]) {
-      correctCounts[2].count++;
-    }
-  });
-
-  correctCounts.sort((a, b) => b.count - a.count);
-
-  const maxCount = correctCounts[0].count;
-
-  // 모든 수포자가 문제를 하나도 맞추지 못한 경우
+export function getHighestScorers(correctCounts) {
+  const maxCount = Math.max(...correctCounts);
   if (maxCount === 0) return [];
 
-  const highestScorers = correctCounts
-    .filter((person) => person.count === maxCount)
-    .map((person) => person.id);
+  return correctCounts.reduce((highestScorers, count, index) => {
+    if (count === maxCount) {
+      highestScorers.push(index + 1); // 인덱스를 수포자 번호로 변환
+    }
+    return highestScorers;
+  }, []);
+}
 
-  return highestScorers;
+export function solution(answers) {
+  const patterns = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 2, 3, 2, 4, 2, 5],
+    [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
+  ];
+
+  const correctCounts = patterns.map((pattern) =>
+    countCorrectAnswers(answers, pattern)
+  );
+
+  return getHighestScorers(correctCounts);
 }
