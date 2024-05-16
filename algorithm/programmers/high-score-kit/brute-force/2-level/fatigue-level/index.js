63d3b8837459d8b606b23ee6eb5385af7e963c2b
seokhochoi <seokho9473@gmail.com>
2024-05-16 22:47:52 +0900

    refactor: 재귀 함수 내부 반복문 제거 -> 배열 내장 메서드 사용, 함수 내부 전역 변수 answer 제거 -> 재귀 호출을 통해 최대 피로도를 반환

--- algorithm/programmers/high-score-kit/brute-force/2-level/fatigue-level/index.js
+++ algorithm/programmers/high-score-kit/brute-force/2-level/fatigue-level/index.js
@@ -1,21 +1,29 @@
 export function solution(k, dungeons) {
-  let answer = 0;
-  const length = dungeons.length;
-  const visited = Array.from({ length }).fill(false);
-
-  const dfs = (fatigue, count) => {
-    for (let i = 0; i < length; i++) {
-      const minFatigue = dungeons[i][0];
-      const consumption = dungeons[i][1];
-      if (!visited[i] && fatigue >= minFatigue) {
-        visited[i] = true;
-        dfs(fatigue - consumption, count + 1);
-        visited[i] = false;
-      }
+  const dfs = (fatigue, count, visited) => {
+    // 더 이상 방문할 수 있는 던전이 없을 때까지 탐색합니다.
+    if (visited.every((isVisited) => isVisited)) {
+      return count;
     }
-    answer = Math.max(answer, count);
-  };
-  dfs(k, 0);
 
-  return answer;
+    let maxCount = count;
+
+    dungeons.forEach(([minFatigue, consumption], index) => {
+      const canVisit = !visited[index] && fatigue >= minFatigue;
+
+      if (canVisit) {
+        visited[index] = true;
+        const result = dfs(fatigue - consumption, count + 1, visited);
+        maxCount = Math.max(maxCount, result);
+        visited[index] = false;
+      }
+    });
+
+    return maxCount;
+  };
+
+  // 던전 방문 여부를 나타내는 배열을 초기화합니다.
+  const visited = Array(dungeons.length).fill(false);
+
+  // DFS 알고리즘을 사용하여 최대 피로도를 계산하고 반환합니다.
+  return dfs(k, 0, visited);
 }
