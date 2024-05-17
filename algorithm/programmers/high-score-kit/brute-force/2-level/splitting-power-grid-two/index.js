a9115338596c2efd2ad5ec35b0331e3b92a877e7
seokhochoi <seokho9473@gmail.com>
2024-05-17 23:18:21 +0900

    refactor: 시멘틱한 이름으로 변경

--- algorithm/programmers/high-score-kit/brute-force/2-level/splitting-power-grid-two/index.js
+++ algorithm/programmers/high-score-kit/brute-force/2-level/splitting-power-grid-two/index.js
@@ -1,8 +1,8 @@
-export function findConnected(array) {
+export function findConnected(wires) {
   const connectedTowers = {};
 
   // 각 숫자를 키로 하는 빈 배열을 생성합니다.
-  array.forEach(([node1, node2]) => {
+  wires.forEach(([node1, node2]) => {
     if (!connectedTowers[node1]) {
       connectedTowers[node1] = [];
     }
@@ -12,7 +12,7 @@ export function findConnected(array) {
   });
 
   // 배열의 요소를 키와 값을 이용하여 그래프 형태로 구성합니다.
-  array.forEach(([node1, node2]) => {
+  wires.forEach(([node1, node2]) => {
     connectedTowers[node1].push(node2);
     connectedTowers[node2].push(node1);
   });
