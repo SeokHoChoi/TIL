6e48ca0e695af0fdd32b20086a4bad21fcc7bcfe
seokhochoi <seokho9473@gmail.com>
2024-05-26 19:56:56 +0900

    refactor: 의미 전달을 위한 선언적  리팩토링 (추후 코드리뷰)

--- algorithm/programmers/high-score-kit/hash/1-level/incomplete-player/index.js
+++ algorithm/programmers/high-score-kit/hash/1-level/incomplete-player/index.js
@@ -1,21 +1,23 @@
 export function solution(participants, completers) {
   const completionCount = {};
-
   // 완주자 명단을 카운트
   completers.forEach((name) => {
-    if (completionCount[name]) {
-      completionCount[name]++;
-    } else {
-      completionCount[name] = 1;
-    }
+    const passedCount = completionCount[name];
+    const isAnyonePassed = !!passedCount;
+    const finalists = isAnyonePassed ? passedCount + 1 : 1;
+
+    completionCount[name] = finalists;
   });
 
   // 참가자 명단을 순회하며 완주하지 못한 사람 찾기
   for (const participant of participants) {
-    if (completionCount[participant]) {
-      completionCount[participant]--;
-    } else {
+    const passdCount = completionCount[participant];
+    const isAnyonePassed = !!passdCount;
+
+    if (!isAnyonePassed) {
       return participant;
     }
+
+    completionCount[participant]--;
   }
 }
