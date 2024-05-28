4bcf49a6c4f91c15f5940189b2d510c78829d77b
seokhochoi <seokho9473@gmail.com>
2024-05-28 21:43:30 +0900

    refactor: 시간 초과 해결

--- algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
+++ algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
@@ -1,13 +1,10 @@
 export function solution(phone_book) {
-  let answer = true;
-  for (let i = 0; i < phone_book.length; i++) {
-    for (let j = 0; j < phone_book.length; j++) {
-      const number = phone_book[i];
-      const len = number.length;
-      if (i !== j && number === phone_book[j].slice(0, len)) {
-        return false;
-      }
+  phone_book.sort();
+  for (let i = 0; i < phone_book.length - 1; i++) {
+    if (phone_book[i + 1].indexOf(phone_book[i]) === 0) {
+      return false;
     }
   }
-  return answer;
+
+  return true;
 }
