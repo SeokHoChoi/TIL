3afc466b5b65db05021296fb34cfb770e9322c6f
seokhochoi <seokho9473@gmail.com>
2024-05-28 21:46:09 +0900

    refactor: 적절한 메서드 이용

--- algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
+++ algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
@@ -1,7 +1,7 @@
 export function solution(phone_book) {
   phone_book.sort();
   for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].indexOf(phone_book[i]) === 0) {
+    if (phone_book[i + 1].startsWith(phone_book[i])) {
       return false;
     }
   }
