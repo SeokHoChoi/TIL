4f6a9939fd1aad69f182c557977c4b12d4d36e36
seokhochoi <seokho9473@gmail.com>
2024-05-28 21:53:06 +0900

    refactor: Hash 사용한 리팩토링 및 중복된 번호 불가인 문제에 맞춘 풀이라 중복된 번호 테스트 케이스 미통과

--- algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
+++ algorithm/programmers/high-score-kit/hash/2-level/telephone-directory/index.js
@@ -1,10 +1,21 @@
 export function solution(phone_book) {
-  phone_book.sort();
-  for (let i = 0; i < phone_book.length - 1; i++) {
-    if (phone_book[i + 1].startsWith(phone_book[i])) {
-      return false;
+  // 해시맵 생성
+  const hash = new Map();
+
+  // 해시맵에 전화번호 추가
+  for (let number of phone_book) {
+    hash.set(number, true);
+  }
+
+  // 각 전화번호의 접두사가 해시맵에 있는지 확인
+  for (let number of phone_book) {
+    for (let i = 1; i < number.length; i++) {
+      if (hash.has(number.slice(0, i))) {
+        return false;
+      }
     }
   }
 
+  // 접두사가 없는 경우
   return true;
 }
