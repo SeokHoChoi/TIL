352491c9ed9385ed5e5729a1149ffc57de041531
seokhochoi <seokho9473@gmail.com>
2024-08-28 22:58:44 +0900

    init: Init wedding-invitation

---
+++ mini-project/wedding-invitation/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Test from './components/Test';
+
+import classNames from 'classnames/bind';
+import styles from './App.module.scss';
+const cx = classNames.bind(styles);
+
+function App() {
+  return (
+    <div className={cx('container')}>
+      <Test />
+    </div>
+  );
+}
+
+export default App;
