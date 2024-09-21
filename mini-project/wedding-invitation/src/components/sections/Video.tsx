ac5870155eca92a8a677b16b47924dac4473f2ec
seokhochoi <seokho9473@gmail.com>
2024-09-21 19:11:57 +0900

    feat: 웨딩 영상 추가

--- mini-project/wedding-invitation/src/components/sections/Video.tsx
+++ mini-project/wedding-invitation/src/components/sections/Video.tsx
@@ -1,13 +1,20 @@
 import classNames from 'classnames/bind';
-import styles from './Heading.module.scss';
+import styles from './Video.module.scss';
 import Section from '@shared/Section';
 
 const cx = classNames.bind(styles);
 
 export default function Video() {
   return (
-    <Section>
-      <h1>Video Component</h1>
+    <Section className={cx('container')}>
+      <video
+        autoPlay={true}
+        muted={true}
+        loop={true}
+        poster="/assets/poster.jpg"
+      >
+        <source src="/assets/wd.mp4" type="video/mp4"></source>
+      </video>
     </Section>
   );
 }
