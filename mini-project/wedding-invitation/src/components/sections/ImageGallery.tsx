d461bcd18f00c8a834b77ebfe252fbda1596747d
seokhochoi <seokho9473@gmail.com>
2024-09-22 19:59:59 +0900

    feat: Gallery Image 배치

---
+++ mini-project/wedding-invitation/src/components/sections/ImageGallery.tsx
@@ -0,0 +1,19 @@
+import classNames from 'classnames/bind';
+import Section from '../shared/Section';
+import styles from './ImageGallery.module.scss';
+
+const cx = classNames.bind(styles);
+
+export default function ImageGallery({ images }: { images: string[] }) {
+  return (
+    <Section title="사진첩">
+      <ul className={cx('wrap-images')}>
+        {images.map((src, idx) => (
+          <li key={idx} className={cx('wrap-image')}>
+            <img src={src} alt="IMAGE" />
+          </li>
+        ))}
+      </ul>
+    </Section>
+  );
+}
